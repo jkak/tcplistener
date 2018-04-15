50abb6c39fed65d2f5c5799f13ea0d7d4765a8cf
jungle85gopy <jungle85gopy@gmail.com>
2018-04-15 23:46:21 +0800

    update tcplistener_test.go for test AcceptTCP()

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # tcplistener
-tcplistener to control the tcp listener
+tcplistener to control the tcp listener。
+
+```go
+type TCPListener struct {
+	tcpListener *net.TCPListener
+	stop        chan bool
+	locked      bool
+	mutex       *sync.Mutex
+}
+```
+
+use stop chan to control the stop of listener.
+
+use mutex to Lock listenter when received stop signal.
+
+
+
+### Test
+
+```shell
+go test
+2018/04/15 23:39:30 Accept() lock status: false
+2018/04/15 23:39:32 -- Accept() err : tcp listener stopped by signal
+2018/04/15 23:39:32 -- failed times: 3; fail rate: 0.000375
+2018/04/15 23:39:32 Accept() lock status: true
+
+2018/04/15 23:39:32 AcceptTCP() lock status: false
+2018/04/15 23:39:42 -- AcceptTCP() err : tcp listener stopped by signal
+2018/04/15 23:39:42 -- failed times: 0; fail rate: 0.000000
+2018/04/15 23:39:42 AcceptTCP() lock status: true
+
+PASS
+ok  	github.com/jungle85gopy/tcplistener	12.499s
+
+```
+
--- tcplistener_test.go
+++ tcplistener_test.go
@@ -7,11 +7,8 @@ import (
 	"time"
 )
 
-var (
-	port = ":8080"
-)
-
 func TestTCPListnerAccept(t *testing.T) {
+	var port = ":8080"
 	var name = "Accept()"
 
 	base, err := net.Listen("tcp", port)
@@ -27,6 +24,76 @@ func TestTCPListnerAccept(t *testing.T) {
 		for {
 			// listener accept request here.
 			newConn, err := tcpLisn.Accept()
+			if err != nil {
+				// I want see the stop signal status
+				log.Printf("-- %s err : %s", name, err)
+				if tcpLisn.locked {
+					break
+				}
+			}
+			// sleep and Close()
+			if newConn != nil {
+				newConn.Close()
+			}
+		}
+	}()
+
+	// request one time to see the locked status.
+	var dia net.Dialer
+	c, err := dia.Dial("tcp", port)
+	if err != nil {
+		t.Logf("%s err : %s", name, err)
+	}
+	network := c.LocalAddr().Network()
+	laddr := *c.LocalAddr().(*net.TCPAddr)
+	defer c.Close()
+	log.Printf("%s lock status: %v\n", name, tcpLisn.locked)
+
+	n := 8000
+	if testing.Short() {
+		n = 100
+	}
+	failed := 0
+	for i := 0; i < n; i++ {
+		var dia net.Dialer
+		dia.Timeout = time.Millisecond * 20
+		c, err := dia.Dial(network, port)
+		if err != nil {
+			t.Logf("current dial should be failed")
+			failed++
+			continue
+		}
+		addr := c.LocalAddr().(*net.TCPAddr)
+		// log.Println("port:", addr.Port, laddr.Port, addr.IP.Equal(laddr.IP))
+		if addr.Port == laddr.Port || !addr.IP.Equal(laddr.IP) {
+			failed++
+			t.Logf("current dial %v should be failed", addr)
+		}
+		c.Close()
+	}
+	tcpLisn.Stop()
+	time.Sleep(time.Millisecond * 10)
+	log.Printf("-- failed times: %d; fail rate: %f\n", failed, float64(failed)/float64(n))
+	log.Printf("%s lock status: %v\n\n", name, tcpLisn.locked)
+}
+
+func TestTCPListnerAcceptTCP(t *testing.T) {
+	var port = ":8080"
+	var name = "AcceptTCP()"
+
+	base, err := net.Listen("tcp", port)
+	if err != nil {
+		t.Fatalf("Listen base err:%s", err)
+	}
+	tcpLisn, err := NewTCPListener(base)
+	if err != nil {
+		t.Fatalf("new Listen err:%s", err)
+	}
+	tcpLisn.Start()
+	go func() {
+		for {
+			// listener accept request here.
+			newConn, err := tcpLisn.AcceptTCP()
 			if err != nil {
 				// I want see the stop signal status
 				log.Printf("-- %s err : %s", name, err)
